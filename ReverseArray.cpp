c1951fbff0d522b55f7bd2a0babc9a3749cb5998
pmpaguio <pmpaguio@csu.fullerton.edu>
2019-02-12 07:18:59 -0800

    Add files via upload

---
+++ ReverseArray.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+void PrintArray()
+{
+	int array[4] = { 1, 2, 3, 4 };
+
+	for (int i = 0; i < array[i]; i++)
+	{
+		cout << array[i] << " ";
+	}
+	cout << endl;
+	for (int i = 3; i >= 0; i--)
+	{
+		cout << array[i] << " ";
+	}
+}
+
+int main()
+{
+	PrintArray();
+	cout << endl;
+	system("pause");
+	return 0;
+}
